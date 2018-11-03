55efe7ca01342b93e056eb1898c44a99a18a131a
Franklin Ye <franklin.q.ye@gmail.com>
2018-11-03 15:00:17 -0700

    first commit

---
+++ note-buddy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  note-buddy
+//
+//  Created by Franklin Ye on 11/3/18.
+//  Copyright © 2018 Franklin Ye. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
