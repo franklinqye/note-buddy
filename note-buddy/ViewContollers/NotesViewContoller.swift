3f15a48f344ceee29a43a3e09ed5afd947811f8d
Franklin Ye <franklin.q.ye@gmail.com>
2018-11-04 00:19:02 -0700

    work pls

---
+++ note-buddy/ViewContollers/NotesViewContoller.swift
@@ -0,0 +1,17 @@
+//
+//  NotesViewContoller.swift
+//  note-buddy
+//
+//  Created by Franklin Ye on 11/4/18.
+//  Copyright © 2018 Franklin Ye. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NotesViewContoller: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
